11e39ae81920edf561002e27ab7d965a9b11750f
702504 <702504@PC432794.cts.com>
2020-05-19 19:52:56 +0530

    Spring JDBC Template

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,16 +1,25 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
+
+	@Autowired
+	private DataSource dataSource;
+
+
 	@Bean
 	public TimeEntryRepository getTimeEntryRepository() {
-		return new InMemoryTimeEntryRepository();
+		return new JdbcTimeEntryRepository(this.dataSource);
 	}
+
 	public static void main(String[] args) {
-        SpringApplication.run(PalTrackerApplication.class, args);
-    }
+		SpringApplication.run(PalTrackerApplication.class, args);
+	}
 }
